23f0fb28d72b658acd31d2a587d0cd229e7d64ab
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-04-19 16:04:05 +0900

    Clean Code

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,18 @@ import { authService } from "../fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj({
           displayName: user.displayName,
           uid: user.uid,
           updateProfile: (args) => user.updateProfile(args),
         });
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
@@ -37,7 +35,7 @@ function App() {
       {init ? (
         <AppRouter
           refreshUser={refreshUser}
-          isLoggedIn={isLoggedIn}
+          isLoggedIn={Boolean(userObj)}
           userObj={userObj}
         />
       ) : (
